efef0185c640ed18c3ff0e174bfce31df1339a8e
inanfatih <mfatihinan@gmail.com>
2019-09-01 01:37:55 -0400

    week7 hw

--- ex.js
+++ ex.js
@@ -1,13 +1,69 @@
-// TODO: define addFavoriteBook(..) function
-
-// TODO: define printFavoriteBooks() function
-
 const favoriteBooks = [];
 
-addFavoriteBook("A Song of Ice and Fire");
-addFavoriteBook("The Great Gatsby");
-addFavoriteBook("Crime & Punishment");
-addFavoriteBook("Great Expectations");
+function addFavoriteBook(bookName) {
+  if (!bookName.includes('Great')) {
+    favoriteBooks.push(bookName);
+  }
+}
+addFavoriteBook('A Song of Ice and Fire');
+addFavoriteBook('The Great Gatsby');
+addFavoriteBook('Crime & Punishment');
+addFavoriteBook('Great Expectations');
 addFavoriteBook("You Don't Know JS");
+function printFavoriteBooks() {
+  console.log('Favorite Books: ' + favoriteBooks.length);
+  for (const i of favoriteBooks) {
+    console.log(i);
+  }
+}
+printFavoriteBooks();
 
-// TODO: print out favorite books
+function sumNumbers(no1, no2, no3) {
+  return no1 + no2 + no3;
+}
+
+function colorCar(color) {
+  console.log('a ' + color + ' car');
+}
+
+function printObject(items) {
+  console.log(JSON.stringify(items));
+}
+
+function vehicleType(color, code) {
+  if (code === 1) {
+    console.log('a ' + color + ' car');
+  } else {
+    console.log('a ' + color + ' motorbike');
+  }
+
+  console.log(3 === 3 ? 'yes' : 'no');
+}
+
+function vehicle(color, code, age) {
+  if (code === 1) {
+    console.log('a ' + color + ' used car');
+  } else {
+    console.log('a ' + color + ' used motorbike');
+  }
+}
+
+const listOfVehicles = [
+  'motorbike',
+  'caravan',
+  'bike',
+  'car',
+  'bus',
+  'train',
+  'plain'
+];
+
+function printThirdItem() {
+  console.log(listOfVehicles[2]);
+}
+
+printThirdItem();
+
+function vehicle(color, code, age) {
+  console.log('a ' + color + ' new ' + listOfVehicles[code - 1]);
+}
