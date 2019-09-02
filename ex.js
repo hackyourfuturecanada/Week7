79ca75aaa362834f6c0a1ede1545676aa781ba92
azizck <omarazi@sheridancollege.ca>
2019-09-02 13:38:45 -0400

    done

--- ex.js
+++ ex.js
@@ -1,13 +1,21 @@
 // TODO: define addFavoriteBook(..) function
-
-// TODO: define printFavoriteBooks() function
+function addFavoriteBook(bookName){
+	if(!bookName.includes("Great"))
+		favoriteBooks.push(bookName);
+}
 
 const favoriteBooks = [];
-
 addFavoriteBook("A Song of Ice and Fire");
 addFavoriteBook("The Great Gatsby");
 addFavoriteBook("Crime & Punishment");
 addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
-
 // TODO: print out favorite books
+
+function printFavoriteBooks(){
+	for(let i = 0; i<favoriteBooks.length; i++){
+	console.log(`Your  ${favoriteBooks.length } favorite books are : ${i+1}. ${favoriteBooks[i]}`)
+	}
+}
+
+printFavoriteBooks();
